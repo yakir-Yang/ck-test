d1d578fb03de36ba25e67dfa2e96bec34c5cc1bf
Kuankuan Yang <kuanyang@tencent.com>
2025-09-21 15:56:01 +0800

    update
    Signed-off-by: Kuankuan Yang <kuanyang@tencent.com>

--- src/main/java/com/example/clickhouse/ClickhouseTestApplication.java
+++ src/main/java/com/example/clickhouse/ClickhouseTestApplication.java
@@ -2,8 +2,9 @@ package com.example.clickhouse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ClickhouseTestApplication {
 
     public static void main(String[] args) {
